5b01eaf25c29b98ccc7dc9d326bfed8549e6ce84
Joeyhiew <joeyhiew@live.com.sg>
2020-03-20 16:44:10 +0800

    Integrating Green Cart

--- src/pages/GreenCart/GreenShoppingCart.js
+++ src/pages/GreenCart/GreenShoppingCart.js
@@ -6,10 +6,11 @@ import './GreenShoppingCart.html'
 import Navbar from '../../components/navbar/Navbar.js'
 var htmlDoc = {__html: GreenShoppingCart2};
 
-
 class GreenShoppingCart extends Component{
     constructor(props){
         super(props);
+        retrieveLocalStorage()
+
     //    this.htmlDoc = require(props.template)
     }
     render(){
@@ -53,6 +54,45 @@ function ready(){
     if(el){
         el.addEventListener('click',recycleClicked);
     }
+
+    
+}
+
+function retrieveLocalStorage(){
+
+    var myData = JSON.parse(localStorage.getItem('objectToPass'));
+    // if (localStorage.hasOwnProperty('objectToPass')){
+    // alert("hass in gc")
+    // }
+    // else{
+    // alert("no in gc")
+    // }
+    
+    if (myData != null){
+        console.log(myData)
+        alert("my data not null")
+        alert(myData)
+        var allData = myData[0];
+
+        if(allData == null){
+            alert("allData is null")
+        }
+        else{
+            alert("alldata not null")
+        }
+        console.log(allData);
+
+        for(var key of allData){
+            alert("key iis")
+            alert(key)
+            var title = key;
+            var points = allData[key][0]; 
+            var img = allData[key][1];
+            addItemToCart(title, points, img);
+        }
+    }
+    window.localStorage.removeItem('objectToPass'); // Clear the localStorage
+
 }
 
 function recycleClicked(event){
@@ -91,16 +131,24 @@ function addToCartClicked(event){
 }
 
 function addItemToCart(title, points, imageSrc){
+    alert(title)
     var cartRow = document.createElement('div')
     cartRow.classList.add('cart-row') //add into the cart horizontally
     var cartItems = document.getElementsByClassName('cart-items')[0]
-    var cartItemNames = cartItems.getElementsByClassName('cart-item-title')
-    for(var i=0; i<cartItemNames.length;i++){
-        if(cartItemNames[i].innerText == title) {
-            alert('This item is already added to the cart')
-            return 
+    if (cartItems == null){
+        alert("cart itmem nulll")
+    }
+    else{
+        alert("cart item not nullll")
+        var cartItemNames = cartItems.getElementsByClassName('cart-item-title')
+        for(var i=0; i<cartItemNames.length;i++){
+            if(cartItemNames[i].innerText == title) {
+                alert('This item is already added to the cart')
+                return 
+            }
         }
     }
+
     var cartRowContents = `
     <div class="cart-item cart-column">
         <img class="cart-item-image" src="${imageSrc}" width="100" height="100">
@@ -112,7 +160,14 @@ function addItemToCart(title, points, imageSrc){
         <button class="btn btn-danger" type="button">REMOVE</button>
     </div>`
     cartRow.innerHTML = cartRowContents //adding the whole html format into the cart
-    cartItems.append(cartRow)
+    if (cartItems == null){
+        cartItems = cartRow
+        alert("cart item=catrt row")
+    }
+    else{
+        cartItems.append(cartRow)
+    }
+    
     //code below is used to make the new remove btn and quantity button works. The original remove and qty btn is already loaded when the page started but not the new buttons
     cartRow.getElementsByClassName('btn-danger')[0].addEventListener('click', removeCartItem)
     cartRow.getElementsByClassName('cart-quantity-input')[0].addEventListener('change', quantityChanged)
